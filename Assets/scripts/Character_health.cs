160fa65bd75e31105e092bb941e8d0b43d2020e2
papaocean <120007868+papaocean@users.noreply.github.com>
2022-12-14 08:59:07 +0100

    snjhs

--- Assets/scripts/Character_health.cs
+++ Assets/scripts/Character_health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_movements : MonoBehaviour
+public class Character_health : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
