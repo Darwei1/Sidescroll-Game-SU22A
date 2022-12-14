e46c485e6266c10cacf2ef2e30668c40983cc2fd
papaocean <120007868+papaocean@users.noreply.github.com>
2022-12-14 09:10:04 +0100

    d

---
+++ Assets/scripts/Characte_controller.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_health : MonoBehaviour
+{
+    public int speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+
+
+    }
+}
